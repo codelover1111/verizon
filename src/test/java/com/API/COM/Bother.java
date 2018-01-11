1e75889dfe724bee35b789bfaa646c2df5f6d27c
fatemaislam <islamfatema04@gmail.com>
2018-01-11 08:44:51 -0500

    Rest Assured file with json

---
+++ src/test/java/com/API/COM/Bother.java
@@ -0,0 +1,69 @@
+package com.API.COM;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
+
+import org.testng.annotations.Test;
+import static com.jayway.restassured.RestAssured.*;
+public class Bother {
+	//Sample get request for getting weather request by city name.
+	//Status code 200.
+@Test
+public void testO() {
+	Response res=when().
+	get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=31545408371126046f9d158c74c859a1");
+    System.out.println(res.getStatusCode());
+    Assert.assertEquals(res.getStatusCode(),200);
+
+
+}
+//Sample get request for getting weather request by city name.
+	//Status code 401.
+@Test
+public void test1() {
+	Response res=when().
+	get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=31545408371126046f9d158c74c859a");
+  System.out.println(res.getStatusCode());
+  Assert.assertEquals(res.getStatusCode(),401);
+
+}
+//parameters with rest assured
+@Test
+public void test2() {
+	Response res=given().
+			param("q","London").
+			param("appid","31545408371126046f9d158c74c859a1").
+			
+			when().
+	get("http://api.openweathermap.org/data/2.5/weather");
+  System.out.println(res.getStatusCode());
+  Assert.assertEquals(res.getStatusCode(),200);
+  if(res.getStatusCode()==200) {
+	  System.out.println("Api is working fine");
+  }else {
+	  System.out.println("Api is not working fine");
+  }
+}
+//Assert our test case in rest assured api
+@Test
+public void test3() {
+	        given().
+			param("q","London").
+			param("appid","31545408371126046f9d158c74c859a1").
+			when().
+	        get("http://api.openweathermap.org/data/2.5/weather").
+	        then().
+	        assertThat().statusCode(200);
+} 
+@Test
+public void test4() {
+	        Response res=given().
+			param("q","London").
+			param("appid","31545408371126046f9d158c74c859a1").
+			when().
+	        get("http://api.openweathermap.org/data/2.5/weather");
+	        System.out.println(res.asString());
+} 
+}
